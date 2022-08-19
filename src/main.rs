3acbbaacd4d7e481b837654dac3c37312f75b700
Anton Gura <an.gura@corp.vk.com>
2022-08-19 19:56:56 +0300

    working proto

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ use walkdir::WalkDir;
 
 fn main() -> std::io::Result<()> {
     // vars
-    let mut _PROJECT_PATH: &str; // project path
-    let mut _PRESCAN_PKG: HashSet<String> = HashSet::new(); // all founded vulnerable packages from prescan
-    let mut _VULN_PKG: HashSet<String> = HashSet::new(); // only vulnerable packages that exist in package.json (explicit dependencies)
-    let mut _IGNORE_PKG: HashSet<String> = HashSet::new(); // all vulnerable packages that not exist in package.json - they should be ignored
+    let mut _project_path: &str; // project path
+    let mut _prescan_pkg: HashSet<String> = HashSet::new(); // all founded vulnerable packages from prescan
+    let mut _vuln_pkg: HashSet<String> = HashSet::new(); // only vulnerable packages that exist in package.json (explicit dependencies)
+    let mut _ignore_pkg: HashSet<String> = HashSet::new(); // all vulnerable packages that not exist in package.json - they should be ignored
 
     /* get args */
     let exp_dep = App::new("trivy-exp-dep")
-        .version("0.1.2")
+        .version("0.1.3")
         .author("Anton Gura <satandyh@yandex.ru>")
         .about("A Trivy plugin that scans the filesystem and skips all packages except for explicitly specified dependencies.")
         .arg(Arg::with_name("path")
@@ -39,12 +39,12 @@ fn main() -> std::io::Result<()> {
             .help("Indicate that all flags after will be passed as trivy global/fs options.\nPositional, should be after \"-p/-h/--\" options."))
         .get_matches();
 
-    _PROJECT_PATH = exp_dep.value_of("path").unwrap();
-    if !path::Path::new(_PROJECT_PATH).is_dir() {
-        eprintln!("No such directory to scan {}", _PROJECT_PATH);
+    _project_path = exp_dep.value_of("path").unwrap();
+    if !path::Path::new(_project_path).is_dir() {
+        eprintln!("No such directory to scan {}", _project_path);
         std::process::exit(1)
     }
-    let global: Vec<&str>;
+    let mut global: Vec<&str>;
 
     /* FIRSTSCAN */
     let prescan = path::Path::new(env::temp_dir().as_path()).join("prescan.json");
@@ -61,7 +61,7 @@ fn main() -> std::io::Result<()> {
         for opt in global {
             firstscan.arg(opt);
         }
-        firstscan.arg(_PROJECT_PATH);
+        firstscan.arg(_project_path);
     } else {
         firstscan.args([
             "fs",
@@ -70,14 +70,14 @@ fn main() -> std::io::Result<()> {
             "json",
             "-o",
             prescan.to_str().unwrap(),
-            _PROJECT_PATH,
+            _project_path,
         ]);
     }
     let firstscanres = firstscan.output()?;
     if !firstscanres.status.success() {
         String::from_utf8(firstscanres.stderr)
             .into_iter()
-            .for_each(|x| eprintln!("{:#?}", x));
+            .for_each(|x| eprint!("{}", x));
         std::process::exit(1);
     }
 
@@ -101,7 +101,7 @@ fn main() -> std::io::Result<()> {
                 .unwrap()
                 .len()
             {
-                _PRESCAN_PKG.insert(
+                _prescan_pkg.insert(
                     prescan_json["Results"][index1]["Vulnerabilities"][index2]["PkgName"]
                         .to_string()
                         .replace('"', ""), // fucking quotes
@@ -111,14 +111,14 @@ fn main() -> std::io::Result<()> {
     } else {
         std::fs::copy(
             prescan.to_str().unwrap(),
-            format!("{}{}", _PROJECT_PATH, "/trivy.json".to_string()),
+            format!("{}{}", _project_path, "/trivy.json".to_string()),
         )?;
         std::fs::remove_file(prescan.to_str().unwrap())?;
         std::process::exit(0);
     }
 
     /* FILTERFIND */
-    for entry in WalkDir::new(_PROJECT_PATH)
+    for entry in WalkDir::new(_project_path)
         .into_iter()
         .filter_map(|e| e.ok())
     {
@@ -134,7 +134,7 @@ fn main() -> std::io::Result<()> {
             let buffered = std::io::BufReader::new(input);
             // stored elements for filter
             let mut exp_set: HashMap<String, Regex> = HashMap::new();
-            for element in _PRESCAN_PKG.clone() {
+            for element in _prescan_pkg.clone() {
                 exp_set.insert(
                     element.to_string(),
                     Regex::new(format!(r"(?i)(^\s*{}\s*=\s*)", element.as_str()).as_str()).unwrap(),
@@ -144,31 +144,65 @@ fn main() -> std::io::Result<()> {
             for line in buffered.lines().filter_map(|x| x.ok()) {
                 for (key, value) in exp_set.iter() {
                     if value.is_match(line.as_str()) {
-                        _VULN_PKG.insert(key.to_string());
+                        _vuln_pkg.insert(key.to_string());
                     }
                 }
             }
         }
     }
-    _IGNORE_PKG = _PRESCAN_PKG
+    _ignore_pkg = _prescan_pkg
         .clone()
-        .difference(&_VULN_PKG.clone())
+        .difference(&_vuln_pkg.clone())
         .cloned()
         .collect();
 
     /* CREATEPOLICY */
     let policy_path = path::Path::new(env::temp_dir().as_path()).join("ignore_policy.rego");
-    let mut output = File::create(policy_path)?;
+    let mut output = File::create(policy_path.clone())?;
     let pola = "package trivy\nimport data.lib.trivy\ndefault ignore = false";
     let polb = format!(
         "ignore_pkgs := {}{}{}",
-        "{",
-        _IGNORE_PKG.into_iter().collect::<Vec<String>>().join(","),
-        "}"
+        "{\"",
+        _ignore_pkg
+            .into_iter()
+            .collect::<Vec<String>>()
+            .join("\",\""),
+        "\"}"
     );
     let polc = "ignore {\ninput.PkgName == ignore_pkgs[_]\n}\n";
     let policy_cotent = [pola, polb.as_str(), polc].join("\n");
     write!(output, "{}", policy_cotent)?;
+
+    /* SCAN */
+    let mut scan = Command::new("trivy");
+    if !exp_dep.value_of("global").is_none() {
+        global = exp_dep.values_of("global").unwrap().collect();
+        scan.arg("fs")
+            .arg("--ignore-policy")
+            .arg(policy_path.clone().as_os_str().to_str().unwrap());
+        for opt in global {
+            scan.arg(opt);
+        }
+        scan.arg(_project_path);
+    } else {
+        scan.args([
+            "fs",
+            "--ignore-policy",
+            policy_path.clone().as_os_str().to_str().unwrap(),
+            _project_path,
+        ]);
+    }
+    let scanres = scan.output()?;
+    if !scanres.status.success() {
+        String::from_utf8(scanres.stderr)
+            .into_iter()
+            .for_each(|x| eprint!("{}", x));
+        std::process::exit(1);
+    } else {
+        String::from_utf8(scanres.stdout)
+            .into_iter()
+            .for_each(|x| print!("{}", x));
+    }
 
     Ok(())
 }
