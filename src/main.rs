2cd649de91c99f87b628a5dc6c8e9d5dfa0f45a0
Anton Gura <an.gura@corp.vk.com>
2022-08-19 17:57:56 +0300

    just big changes in my mind

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use clap::{App, Arg};
-//use regex::Regex;
+use regex::{self, Regex};
 use serde_json::Value;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::io::BufRead;
 use std::path;
 use std::process::Command;
 use walkdir::WalkDir;
 
 fn main() -> std::io::Result<()> {
+    // vars
+    let mut _PROJECT_PATH: &str; // project path
+    let mut _PRESCAN_PKG: HashSet<String> = HashSet::new(); // all founded vulnerable packages from prescan
+    let mut _VULN_PKG: HashSet<String> = HashSet::new(); // only vulnerable packages that exist in package.json (explicit dependencies)
+    let mut _IGNORE_PKG: HashSet<String> = HashSet::new(); // all vulnerable packages that not exist in package.json - they should be ignored
+
     let exp_dep = App::new("trivy-exp-dep")
         .version("0.1.2")
         .author("Anton Gura <satandyh@yandex.ru>")
@@ -29,14 +37,14 @@ fn main() -> std::io::Result<()> {
             .help("Indicate that all flags after will be passed as trivy global/fs options.\nPositional, should be after \"-p/-h/--\" options."))
         .get_matches();
 
-    let project_path = exp_dep.value_of("path").unwrap();
-    if !path::Path::new(project_path).is_dir() {
-        eprintln!("No such directory to scan {}", project_path);
+    _PROJECT_PATH = exp_dep.value_of("path").unwrap();
+    if !path::Path::new(_PROJECT_PATH).is_dir() {
+        eprintln!("No such directory to scan {}", _PROJECT_PATH);
         std::process::exit(1)
     }
     let global: Vec<&str>;
 
-    // firstscan
+    /* FIRSTSCAN */
     let prescan = path::Path::new(env::temp_dir().as_path()).join("prescan.json");
     let mut firstscan = Command::new("trivy");
     if !exp_dep.value_of("global").is_none() {
@@ -51,7 +59,7 @@ fn main() -> std::io::Result<()> {
         for opt in global {
             firstscan.arg(opt);
         }
-        firstscan.arg(project_path);
+        firstscan.arg(_PROJECT_PATH);
     } else {
         firstscan.args([
             "fs",
@@ -60,7 +68,7 @@ fn main() -> std::io::Result<()> {
             "json",
             "-o",
             prescan.to_str().unwrap(),
-            project_path,
+            _PROJECT_PATH,
         ]);
     }
     let firstscanres = firstscan.output()?;
@@ -71,7 +79,7 @@ fn main() -> std::io::Result<()> {
         std::process::exit(1);
     }
 
-    // findfiles
+    /* FINDFILES */
     if !path::Path::new(prescan.to_str().unwrap()).exists() {
         eprintln!(
             "No such file or it's can't be read {}",
@@ -84,7 +92,6 @@ fn main() -> std::io::Result<()> {
         serde_json::from_str::<Value>(&jsondata).unwrap()
     };
 
-    let mut prescan_pkg = Vec::new();
     if prescan_json.get("Results") != None {
         for index1 in 0..prescan_json["Results"].as_array().unwrap().len() {
             for index2 in 0..prescan_json["Results"][index1]["Vulnerabilities"]
@@ -92,39 +99,68 @@ fn main() -> std::io::Result<()> {
                 .unwrap()
                 .len()
             {
-                prescan_pkg.push(
+                _PRESCAN_PKG.insert(
                     prescan_json["Results"][index1]["Vulnerabilities"][index2]["PkgName"]
-                        .as_str()
-                        .unwrap(),
+                        .to_string()
+                        .replace('"', ""), // fucking quotes
                 );
             }
         }
-        prescan_pkg.dedup();
-        println!("{}", prescan_pkg.join(" "));
     } else {
-        // the same as if prescan_pkg.len() == 0
+        // the same as if _PRESCAN_PKG.len() == 0
         std::fs::copy(
             prescan.to_str().unwrap(),
-            format!("{}{}", project_path, "/trivy.json".to_string()),
+            format!("{}{}", _PROJECT_PATH, "/trivy.json".to_string()),
         )?;
         std::fs::remove_file(prescan.to_str().unwrap())?;
         std::process::exit(0);
     }
+    // check
+    for i in _PRESCAN_PKG.clone() {
+        println!("--> {}", i);
+    }
 
-    // filterfind
-    for entry in WalkDir::new(project_path)
+    /* FILTERFIND */
+    for entry in WalkDir::new(_PROJECT_PATH)
         .into_iter()
         .filter_map(|e| e.ok())
     {
+        //case insensitive by lowercase
         if entry
             .file_name()
             .to_ascii_lowercase()
             .to_string_lossy()
             .ends_with("pipfile")
         {
-            println!("{}", entry.path().to_string_lossy());
+            // read file
+            let input = std::fs::File::open(entry.path())?;
+            let buffered = std::io::BufReader::new(input);
+            // stored elements for filter
+            let mut exp_set: HashMap<String, Regex> = HashMap::new();
+            for element in _PRESCAN_PKG.clone() {
+                exp_set.insert(
+                    element.to_string(),
+                    regex::Regex::new(format!(r"(?i)(^\s*{}\s*=\s*)", element.as_str()).as_str())
+                        .unwrap(),
+                );
+            }
+            // filter thru all regex
+            for line in buffered.lines().filter_map(|x| x.ok()) {
+                for (key, value) in exp_set.iter() {
+                    if value.is_match(line.as_str()) {
+                        _VULN_PKG.insert(key.to_string());
+                    }
+                }
+            }
         }
     }
-
+    // check
+    for j in _VULN_PKG.clone() {
+        println!("==> {}", j);
+    }
+    //_IGNORE_PKG = _PRESCAN_PKG.clone().difference(&_VULN_PKG.clone()).collect()::<HashSet<String>>();
+    //for k in _IGNORE_PKG.clone() {
+    //    println!("++> {}", k);
+    //}
     Ok(())
 }
